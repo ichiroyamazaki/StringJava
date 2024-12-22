bb30f8661920c7a74a32b0e32ba9e395822f5832
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 00:40:46 +0800

    Create Group3Activity.java

---
+++ Group3Activity.java
@@ -0,0 +1,13 @@
+public class Group3Activity
+{
+	public static void main(String[] args) {
+
+	String first = "Ichiro Yamazaki";
+	String second = "19";
+	String third = "Gaming";
+	
+	System.out.println(first);
+	System.out.println(second);
+	System.out.println(third);
+	}
+}
